b906ae05649d6517491a824a27223d2d8e9b7675
K1ndoctop <argen.paiysbekov04@gmail.com>
2023-10-02 14:16:45 +0600

    navbar

--- src/store/account/accountActions.js
+++ src/store/account/accountActions.js
@@ -1,15 +1,27 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
-import axios from 'axios';
-import { ACCOUNT_API } from '../../helpers/consts';
+import { ACCOUNT_API } from "../../helpers/consts";
+import axios from "axios";
 
 export const registerAccount = createAsyncThunk(
-    'account/registerAccount',
-    async ({ user, navigate }) => {
-        const accountData = new FormData();
-        accountData.append('username', user.username);
-        accountData.append('password', user.password);
-        const res = await axios.post(`${ACCOUNT_API}/register/`, accountData);
-        console.log(res);
-        return { navigate };
-    }
+  "account/registerAccount",
+  async ({ user, navigate }) => {
+    const accountData = new FormData();
+    accountData.append("username", user.username);
+    accountData.append("password", user.password);
+    const res = await axios.post(`${ACCOUNT_API}/register/`, accountData);
+    console.log(res);
+    return { navigate };
+  }
 );
+export const loginAccount = createAsyncThunk( 
+  "account/loginAccount", 
+  async ({ user, navigate }) => { 
+    const accountData = new FormData(); 
+    accountData.append("username", user.username); 
+    accountData.append("password", user.password); 
+    await axios.post(`${ACCOUNT_API}/api/token/`, accountData); 
+    return { navigate, user: user.username }; 
+  } 
+);
+
+
