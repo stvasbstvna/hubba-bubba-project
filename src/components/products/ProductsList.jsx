a8eaa182a0179b0aa4cb0874f376d17848464277
AmanNookat <138446723+AmanNookat@users.noreply.github.com>
2023-10-06 10:50:51 +0600

    pagination-style

--- src/components/products/ProductsList.jsx
+++ src/components/products/ProductsList.jsx
@@ -1,34 +1,34 @@
-import React,{useEffect} from 'react'
-import { useDispatch,useSelector } from 'react-redux'
-import { getProducts } from '../../store/products/productsActions'
-import ProductItem from './ProductItem';
-import ProductsPagination from './ProductsPagination';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getProducts } from "../../store/products/productsActions";
+import ProductItem from "./ProductItem";
+import ProductsPagination from "./ProductsPagination";
 
 const ProductsList = () => {
-    const {products, loading } = useSelector(state => state.products)
-    const dispacth = useDispatch()
+  const { products, loading } = useSelector((state) => state.products);
+  const dispacth = useDispatch();
 
-    useEffect(() =>{
-        dispacth(getProducts())
-    },[])
+  useEffect(() => {
+    dispacth(getProducts());
+  }, []);
 
   return (
     <>
-        {loading ? (
-            <h3>Loading...</h3>
-        ):(
-            <div className='flex flex-row flex-wrap justify-center'>
-            {
-                products.map(products =>(
-                    <ProductItem key={products.id} product={products}/>
-                ))
-            }
-            <ProductsPagination />
-            </div>
-        )}
-
+      {loading ? (
+        <h3>Loading...</h3>
+      ) : (
+        <div className="flex flex-col items-center">
+          <ProductsPagination />
+          <div className="flex flex-wrap justify-center">
+            {products.map((products) => (
+              <ProductItem key={products.id} product={products} />
+            ))}
+          </div>
+          <ProductsPagination />
+        </div>
+      )}
     </>
-  )
-}
+  );
+};
 
-export default ProductsList
+export default ProductsList;
