dd0d110c873804182802a1c643cd98439b78f401
Sabina <sabina@MacBooks-MacBook-Air.local>
2023-10-26 16:55:15 +0600

    commit

--- src/helpers/functions.js
+++ src/helpers/functions.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 import { ACCOUNT_API } from './consts';
 
-
 export const addDataToLocalStorage = (user, tokens) => {
     localStorage.setItem('user', JSON.stringify(user));
     localStorage.setItem('tokens', JSON.stringify(tokens));
